43d9945d2a1a636ee350d32fdf4fd9f4ee1b8283
Arnau Brugat <abrugat@cendrasso.net>
2026-06-08 23:54:58 +0200

    Peixera Fet

---
+++ obj/Debug/net9.0/RecuPeixera.AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("RecuPeixera")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("RecuPeixera")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RecuPeixera")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
